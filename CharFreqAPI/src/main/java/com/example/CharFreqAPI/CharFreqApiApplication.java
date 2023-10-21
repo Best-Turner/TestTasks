21f2b1fe7ff52e4cdc5638df0c56282e6b93f1d8
Best-Turner <129537829+Best-Turner@users.noreply.github.com>
2023-10-21 14:07:13 +0300

    create

---
+++ CharFreqAPI/src/main/java/com/example/CharFreqAPI/CharFreqApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.CharFreqAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharFreqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharFreqApiApplication.class, args);
+	}
+
+}
