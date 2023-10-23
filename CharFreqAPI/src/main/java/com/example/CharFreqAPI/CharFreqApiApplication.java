90bb22b2e240bd1f67216f78bbc9ec22c2548f62
Best-Turner <shurikx6881@gmail.com>
2023-10-23 20:12:08 +0300

    /

--- CharFreqAPI/src/main/java/com/example/CharFreqAPI/CharFreqApiApplication.java
+++ CharFreqAPI/src/main/java/com/example/CharFreqAPI/CharFreqApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CharFreqApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CharFreqApiApplication.class, args);
     }
