917e3ec625f33d746225832f55964e8b1f05a790
unknown <shurikx6881@gmail.com>
2023-12-07 19:53:59 +0300

    Initial commit: Set up Spring Boot project

---
+++ TaskManagementSystem/src/test/java/ru/effective/mobile/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.effective.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
